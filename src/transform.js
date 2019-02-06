60367e081ed32da21b6c3ba1ae088b16bcd58977
Felix Laukel <felix@laukel.com>
2019-02-06 15:45:49 +0100

    Avoid spread operator in transform* functions

--- src/transform.js
+++ src/transform.js
@@ -1,15 +1,17 @@
+/* eslint-disable  no-param-reassign */
+
 export function transformSelectors ({ state }, originalSelectors) {
     return Object.entries(originalSelectors)
-        .reduce((selectors, [key, fn]) => ({
-            ...selectors,
-            [key]: fn(state),
-        }), {});
+        .reduce((selectors, [key, fn]) => {
+            selectors[key] = fn(state);
+            return selectors;
+        }, {});
 }
 
 export function transformActions ({ dispatch }, originalActions) {
     return Object.entries(originalActions)
-        .reduce((actions, [key, fn]) => ({
-            ...actions,
-            [key]: (...args) => dispatch(fn(...args)),
-        }), {});
+        .reduce((actions, [key, fn]) => {
+            actions[key] = (...args) => dispatch(fn(...args));
+            return actions;
+        }, {});
 }
